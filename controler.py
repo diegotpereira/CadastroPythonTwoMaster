1ac1b5364287feffb3725b57ab444215ada98084
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-12-18 14:46:47 -0300

    alteração no arquivo classe .py adicionado metodo inserir no banco de dados

--- controler.py
+++ controler.py
@@ -1,4 +1,12 @@
 from PyQt5 import uic, QtWidgets
+import mysql.connector
+
+banco = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="db_cadastro_produtos"
+)
 
 
 def funcao_principal():
@@ -7,22 +15,49 @@ def funcao_principal():
     linha3 = formulario.lineEdit_3.text()
     linha4 = formulario.lineEdit_4.text()
 
+    categoria = ""
+
     if formulario.radioButton.isChecked():
         print("Categoria Eletronicos selecionada")
+        categoria = "Eletronicos"
     elif formulario.radioButton_2.isChecked():
         print("Categoria Informatica selecionada")
+        categoria = "Informatica"
     else:
         print("Categoria Alimentos selecionada")
+        categoria = "Alimentos"
 
-    print("Código:", linha1)
+    print("Codigo:", linha1)
     print("Descricao:", linha2)
     print("Preco", linha3)
     print("Fabricante", linha4)
 
+    cursor = banco.cursor()
+    comando_SQL = "INSERT INTO produtos (codigo,descricao,preco, fabricante, categoria) VALUES (%s,%s,%s,%s,%s)"
+    dados = (str(linha1), str(linha2), str(linha3), str(linha4), categoria)
+    cursor.execute(comando_SQL, dados)
+    banco.commit()
+
 
 app = QtWidgets.QApplication([])
-formulario = uic.loadUi("formulario2.ui")
+formulario = uic.loadUi("formulario.ui")
 formulario.pushButton.clicked.connect(funcao_principal)
 
 formulario.show()
 app.exec()
+
+
+# criando a tabela
+
+""" create table produtos (id INT NOT NULL AUTO_INCREMENT,
+codigo INT,
+descricao VARCHAR(50),  
+preco DOUBLE,
+fabricante VARCHAR(50), 
+categoria VARCHAR(20),
+PRIMARY KEY (id)
+);  """
+
+# inserindo registros na tabela
+
+#INSERT INTO produtos (codigo,descricao,preco,fabricante, categoria) VALUES (123,"impressora",500.00, "xerox", "informatica");
